108356a63e58d16e66816c1002720643cb1e3eae
thejsj <jorge.silva@thejsj.com>
2016-11-08 15:02:58 -0800

    Add requirements. Add Postgresql

--- django_starter/settings.py
+++ django_starter/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_starter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_starter',
+        'USER': 'django_starter',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
