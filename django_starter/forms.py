2f75a850e8fe020c22607630b2667530cc0b8550
tosih <sohail@runnable.com>
2016-11-21 23:44:44 -0800

    Update dockerfile/docker-compose for live development, add bulma css framework, change html structure.

--- django_starter/forms.py
+++ django_starter/forms.py
@@ -2,6 +2,6 @@ from django.forms import ModelForm
 from django_starter.models import Task
 
 class TaskForm(ModelForm):
-    class Meta:
-       model = Task
-       fields = ['content']
+  class Meta:
+    model = Task
+    fields = ['content']
